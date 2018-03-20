0d91a3504033ba27a3923cbe2efe1a0f06c58493
Andrea Telatin <andrea@telatin.com>
2018-03-20 15:45:30 +0000

    Fixed deprecation; Added 'comment' retention from sequence names (e.g. Illumina Index)

--- readfq.pl
+++ readfq.pl
@@ -4,46 +4,47 @@ use strict;
 use warnings;
 
 sub readfq {
-	my ($fh, $aux) = @_;
-	@$aux = [undef, 0] if (!defined(@$aux));
-	return if ($aux->[1]);
-	if (!defined($aux->[0])) {
-		while (<$fh>) {
-			chomp;
-			if (substr($_, 0, 1) eq '>' || substr($_, 0, 1) eq '@') {
-				$aux->[0] = $_;
-				last;
-			}
-		}
-		if (!defined($aux->[0])) {
-			$aux->[1] = 1;
-			return;
-		}
-	}
-	my $name = /^.(\S+)/? $1 : '';
-	my $seq = '';
-	my $c;
-	$aux->[0] = undef;
-	while (<$fh>) {
-		chomp;
-		$c = substr($_, 0, 1);
-		last if ($c eq '>' || $c eq '@' || $c eq '+');
-		$seq .= $_;
-	}
-	$aux->[0] = $_;
-	$aux->[1] = 1 if (!defined($aux->[0]));
-	return ($name, $seq) if ($c ne '+');
-	my $qual = '';
-	while (<$fh>) {
-		chomp;
-		$qual .= $_;
-		if (length($qual) >= length($seq)) {
-			$aux->[0] = undef;
-			return ($name, $seq, $qual);
-		}
-	}
-	$aux->[1] = 1;
-	return ($name, $seq);
+    my ($fh, $aux) = @_;
+    @$aux = [undef, 0] if (!(@$aux));	# remove deprecated 'defined'
+    return if ($aux->[1]);
+    if (!defined($aux->[0])) {
+        while (<$fh>) {
+            chomp;
+            if (substr($_, 0, 1) eq '>' || substr($_, 0, 1) eq '@') {
+                $aux->[0] = $_;
+                last;
+            }
+        }
+        if (!defined($aux->[0])) {
+            $aux->[1] = 1;
+            return;
+        }
+    }
+    my $name = /^.(\S+)/? $1 : '';
+    my $comm = /^.\S+\s+(.*)/? $1 : ''; # retain "comment"
+    my $seq = '';
+    my $c;
+    $aux->[0] = undef;
+    while (<$fh>) {
+        chomp;
+        $c = substr($_, 0, 1);
+        last if ($c eq '>' || $c eq '@' || $c eq '+');
+        $seq .= $_;
+    }
+    $aux->[0] = $_;
+    $aux->[1] = 1 if (!defined($aux->[0]));
+    return ($name, $seq) if ($c ne '+');
+    my $qual = '';
+    while (<$fh>) {
+        chomp;
+        $qual .= $_;
+        if (length($qual) >= length($seq)) {
+            $aux->[0] = undef;
+            return ($name, $seq, $comm, $qual);
+        }
+    }
+    $aux->[1] = 1;
+    return ($name, $seq, $comm);
 }
 
 my @aux = undef;
